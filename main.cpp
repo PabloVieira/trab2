1619f41bad29118a44984a9ab0d8aeda093428ac
Pablo Vieira <pablo.vieira@edu.pucrs.br>
2020-11-03 21:23:53 -0300

    includes <thread>, <chrono> etc

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <iostream>       // std::cout, std::endl
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
 
 using namespace std;
 
